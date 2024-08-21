576a9d143c08131de06d6c5220753e757544d325
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-08-21 21:00:10 +0530

    add next js config for images

--- innovation/next.config.mjs
+++ innovation/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // domains: ["https://picsum.photos/200"],
+        // domains: ["https://nextjs.org/docs/messages/next-image-unconfigured-host"],
+      },
+};
 
 export default nextConfig;
