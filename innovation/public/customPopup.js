4a2547b302d8b16cbab5c1be2875aa99dd6d6576
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-08-31 08:51:12 +0530

    Add custom popup js file for testing

---
+++ innovation/public/customPopup.js
@@ -0,0 +1,22 @@
+const customPopup = () => {
+//   const para = document.createElement("p");
+//   para.setAttribute("id", "customPopupMessage");
+//   para.setAttribute("class", "customMessageClass");
+//   const node = document.createTextNode("This is new.");
+//   para.appendChild(node);
+
+//   const element = document.getElementById("div1");
+//   element.appendChild(para);
+
+//   let elems = document.querySelectorAll(".customMessageClass");
+//   for (const e of elems) {
+//     setTimeout(
+//       function (o) {
+//         //2. Receive the element in variable o
+//         o.remove(); //3. Remove the o variable
+//       },
+//       5000,
+//       e
+//     );
+//   }
+};
