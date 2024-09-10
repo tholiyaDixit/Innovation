b3efb2a24e73db5283ff05868d4a30b0c92721ff
tholiyaDixit <tholiyadixit1996@gmail.com>
2024-09-10 20:14:22 +0530

    Add image and create convertImage page

---
+++ innovation/src/app/images/convertImage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConvertImage = () => {
+  return (
+    <div>
+      ConvertImage page
+    </div>
+  )
+}
+
+export default ConvertImage
--- innovation/src/app/images/page.tsx
+++ innovation/src/app/images/page.tsx
@@ -2,6 +2,7 @@ import ImagePageCard from "@/components/card/imagePageCard/ImagePageCard";
 import React from "react";
 import sizeReduce from "../../images/sizeReduce.png";
 import base64Decode from "../../images/base64-decode.png";
+import convertImage from "../../images/convertImage.png";
 
 interface Props {}
 
@@ -19,15 +20,16 @@ const page = (props: Props) => {
         <ImagePageCard
           price={"Free"}
           description={`click 'Upload' button and select any image then wait for base64 decode result`}
-          title={"base 64"}
+          title={"Base 64"}
           img={base64Decode}
           navigate={"/images/base%64%decode"}
         />
         <ImagePageCard
           price={"Free"}
           description={`click 'Upload' button and select any image then wait for result`}
-          title={"Reduce File Size"}
-          navigate={"/"}
+          title={"Convert Image"}
+          img={convertImage}
+          navigate={"/images/convertImage"}
         />
         <ImagePageCard
           price={"Free"}
